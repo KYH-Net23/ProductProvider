d38d6c812089dccc3a160c8cbd500fc6bb5ada74
Gustav Ivering <gustav.ivering@live.se>
2024-11-04 10:06:38 +0100

    Added ProductEntity model

---
+++ Shared/Models/ProductEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+    public class ProductEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? Category { get; set; }
+        public string Image { get; set; } = null!;
+        public int Stock { get; set; }
+        public string? Size { get; set; }
+        public int MyProperty { get; set; }
+        public DateOnly AddedDate { get; set; }
+    }
+}

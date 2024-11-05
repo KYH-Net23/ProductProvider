b82b0a64932e410c1232bf393710fbdeadd964e5
Fittgross <william-gross@hotmail.com>
2024-11-05 12:23:49 +0100

    Added a ProductViewModelFactory

---
+++ ProductsReadAll/Factories/ProductViewModelFactory.cs
@@ -0,0 +1,25 @@
+﻿using ProductsReadAll.ViewModels;
+using Shared.Models;
+
+namespace ProductsReadAll.Factories
+{
+    public static class ProductViewModelFactory
+    {
+        public static List<ProductViewModel> GetProducts(List<ProductEntity> products)
+        {
+            return products.Select(GetProduct).ToList();
+        }
+
+        public static ProductViewModel GetProduct(ProductEntity product)
+        {
+            return new ProductViewModel
+            {
+                Id = product.Id,
+                Brand = product.Brand,
+                Model = product.Model,
+                Price = product.Price,
+                Image = product.Image
+            };
+        }
+    }
+}

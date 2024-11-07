8e4ae817aff92040a5a9ab7e8a308e6426c95cf7
Kicker <Kicker@ABDINDATOR>
2024-11-07 12:03:33 +0100

    added my api to dev

---
+++ ProductsDelete/Controllers/ProductsDeleteController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductsDelete.Data;
+
+namespace ProductsDelete
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductDeleteController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ProductDeleteController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _context.products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.products.Remove(product);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}

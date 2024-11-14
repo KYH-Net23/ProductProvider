9cd99e115dfdc7fe6c818b269756ab900698f94b
hmacker <chammas_mousa@live.se>
2024-11-14 09:38:27 +0100

    Created new class for integrationTests for Search Controller

---
+++ ProductProvider.Tests/Controllers/SearchProductsControllerIntegrationTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductProvider.Tests.Controllers
+{
+    public class SearchProductsControllerIntegrationTests
+    {
+        private WebApplicationFactory<Program> _factory;
+        private HttpClient _client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _factory = new WebApplicationFactory<Program>();
+            _client = _factory.CreateClient();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _factory.Dispose();
+            _client.Dispose();
+        }
+
+    }
+}
